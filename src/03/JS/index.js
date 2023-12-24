70a5bf2e50cda86e01a5bdc64b82da6ddebada66
Ana <anasouza673@gmail.com>
2023-12-24 11:47:58 -0300

    localizado numero em uma array

---
+++ src/03/JS/index.js
@@ -0,0 +1,39 @@
+let verificar = document.querySelector('#verificar')
+let numero = document.querySelector('#numero')
+let numVerifi = document.querySelector('#numVerifi')
+let res = document.querySelector('#res')
+
+verificar.addEventListener('click', verificarArray)
+let array = []
+function verificarArray(){
+    array.push(numero.value)
+    let localizarNumero = Number(numVerifi.value)
+    console.log(array)
+
+
+    const setUnico = new Set(array);
+    const deVoltaAAray = [...setUnico];
+
+    let arrayDeNumero = deVoltaAAray.join(', ').toString()
+                                    .replaceAll(',','').split(' ')
+                                    .map(str => parseInt(str, 10))
+
+    
+
+    console.log(arrayDeNumero)
+
+    const setUnico2 = new Set(arrayDeNumero);
+    const deVoltaAAray2 = [...setUnico2];
+    console.log(deVoltaAAray2)
+    let localizar = deVoltaAAray2.indexOf(localizarNumero) + 1
+
+    console.log(localizar)
+
+    if(localizar === 0){
+        res.innerHTML = `Numero não localizado`
+    }else{
+        res.innerHTML = `Numero localizado na posição: ${localizar}`
+    }
+
+}
+
